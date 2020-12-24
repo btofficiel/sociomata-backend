e89df152569b19c0e5d98b469c247553106c0b53
Bhaskar Thakur <bhaskar@collobe.com>
2020-12-24 19:29:02 +0530

    Add env variables retrieval to before.sh

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 cd /home/ubuntu/backend
+rm -rf .env
 npm install
+if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]
+then
+    echo "Retrieving environment variables..."
+    echo DB_HOST=$(aws ssm get-parameter --name sociomata-staging-db_host --with-decryption --query Parameter.Value) >> .env
+    echo DB_USER=$(aws ssm get-parameter --name sociomata-staging-db_user --with-decryption --query Parameter.Value) >> .env
+    echo DB_PASS=$(aws ssm get-parameter --name sociomata-staging-db_pass --with-decryption --query Parameter.Value) >> .env
+    echo DB_PORT=5432 >> .env
+    echo DB_NAME=$(aws ssm get-parameter --name sociomata-staging-db --with-decryption --query Parameter.Value) >> .env
+    echo TWITTER_APIKEY=$(aws ssm get-parameter --name sociomata-twitter_apikey --with-decryption --query Parameter.Value) >> .env
+    echo TWITTER_SECRET=$(aws ssm get-parameter --name sociomata-twitter_secret --with-decryption --query Parameter.Value) >> .env
+    echo TWITTER_PASSWORD=$(aws ssm get-parameter --name sociomata-staging-twitter_pass --with-decryption --query Parameter.Value) >> .env
+    echo LOG_DIRECTORY="../backend-logs" >> .env
+    echo ENV=staging >> .env
+    echo "Environment variables retrieved successfully"
+else
+    echo "Production not setup yet"
+fi
+

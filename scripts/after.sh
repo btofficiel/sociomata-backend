f922191671588c7a5963dbc87d4d7f433a5324f7
Bhaskar Thakur <bhaskar@collobe.com>
2021-10-12 20:37:05 +0530

    Fix 403 error on presigned posts

--- scripts/after.sh
+++ scripts/after.sh
@@ -6,6 +6,8 @@ sudo npm install
 if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]
 then
     echo "Retrieving environment variables..."
+    echo AWS_PRESIGNED_ID=$(aws ssm get-parameter --name staging-presigned_id --with-decryption --query Parameter.Value) >> .env
+    echo AWS_PRESIGNED_KEY=$(aws ssm get-parameter --name staging-presigned_key --with-decryption --query Parameter.Value) >> .env
     echo DB_HOST=$(aws ssm get-parameter --name sociomata-staging-db_host --with-decryption --query Parameter.Value) >> .env
     echo DB_USER=$(aws ssm get-parameter --name sociomata-staging-db_user --with-decryption --query Parameter.Value) >> .env
     echo DB_PASS=$(aws ssm get-parameter --name sociomata-staging-db_pass --with-decryption --query Parameter.Value) >> .env

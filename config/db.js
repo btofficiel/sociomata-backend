7b94a7db033c8867f7d45ecd1c1cae8751c10768
Bhaskar Thakur <bhaskar@collobe.com>
2022-01-17 16:48:18 +0530

    Fix SSL config in pgPromise

--- config/db.js
+++ config/db.js
@@ -1,12 +1,25 @@
 const pgp = require('pg-promise')();
 
+const configureSSL = () => {
+    switch(process.env.ENV) {
+        case "dev":
+            return false;
+        case "prod":
+            return {rejectUnauthorized: false};
+        case "staging":
+            return true;
+        default:
+            return false;
+    }
+};
+
 const dbConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    ssl: process.env.ENV !== "dev",
+    ssl: configureSSL(),
     allowExitOnIdle: true
 };
 
